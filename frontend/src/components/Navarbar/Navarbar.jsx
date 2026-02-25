89d77305594fa6e3ecf2da25ffdf3fbcbe61bfa0
Getacher-Kifilie <getacherkifilie23@gmail.com>
2026-02-25 21:01:37 +0300

    add image folder  for this website

---
+++ frontend/src/components/Navarbar/Navarbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navarbar.css';
+import {assets} from '../../assets/assets.js'
+const Navarbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={} alt="" />
+    </div>
+  )
+}
+
+export default Navarbar
