912f17df2b4f3a41ca9fadd7445980b8807830ed
Getacher-Kifilie <getacherkifilie23@gmail.com>
2026-02-25 22:41:28 +0300

    create some neccessary file ind starting code home pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import React from 'react';
+import { Route,Routes } from 'react-router-dom';
+import Navarbar from './components/Navarbar/Navarbar.jsx';
+import Home from './pages/Home/Home.jsx';
+import Cart from './pages/Home/Cart.jsx';
+import PlaceOrder from './pages/Home/PlaceOrder.jsx';
 const App = () => {
   return (
-    <div>App</div>
+   <div className='app'>
+   <Navarbar/>
+   <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/placeOrder' element={<PlaceOrder/>}/>
+   </Routes>
+   
+   </div>
   )
 }
 
---
+++ frontend/src/components/Navarbar/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Orders Your favorite food here</h2>
+           
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi qui pariatur voluptas reiciendis id sed minima laboriosam voluptatem deleniti impedit, aperiam, nihil magnam maxime voluptate reprehenderit est ducimus eaque culpa!</p>
+        <button>
+            View Menu
+        </button>
+         </div>
+    </div>
+  )
+}
+
+export default Header
