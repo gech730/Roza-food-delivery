cf794e9d90c6838f4b53173bc8a8c907e8dca7e7
Getacher-Kifilie <getacherkifilie23@gmail.com>
2026-02-26 10:27:51 +0300

    style food -tem

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import { food_list } from "../assets/assets";
-export const StoreContext=createContext(null);
+ export const StoreContext=createContext(null);
 const StoreContextProvider=(props)=>{
   const contextValue={
    food_list
