f26cd7fc62f0d0fe57188b80985f259236fd6379
Getacher-Kifilie <getacherkifilie23@gmail.com>
2026-03-02 14:00:07 +0300

    add some icon

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -2,13 +2,16 @@ import order_icon from './order_icon.png'
 import logo from './logo.png'
 import parcel_icon from './parcel_icon.png'
 import profile_icon from './profile_icon.png'
-import upload_area from './apload-area.png'
-
+import upload_area from './upload-area.png'
+import add_icon from './add_icon_white.png'
+import cross_icon from './cross_icon.png'
 export const assets = {
     order_icon,
     profile_icon,
     logo,
     parcel_icon,
-    upload_area
+    upload_area,
+    add_icon,
+    cross_icon
 }
 
