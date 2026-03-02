d99ba6e0243c1fde3eea7065057574972e0d15b2
Getacher-Kifilie <getacherkifilie23@gmail.com>
2026-03-02 16:11:42 +0300

    make neat add page

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 import './Add.css'
+import { assets } from '../../assets/assets'
 const Add = () => {
   return (
-    <div>Add</div>
+    <div className='add'>
+        <form className='flex-col'>
+            <div className="add-img-upload flex-col">
+                    <p>Upload Image</p>
+                    <label htmlFor="image">
+                        <img src={assets.upload_area} alt="" />
+                    </label>
+                    <input type="file" id='image' hidden required />
+            </div>
+            <div className="add-product-name flex-col">
+                <p>Product name</p>
+                <input type="text" name='name' placeholder='type here' />
+            </div>
+            <div className="add-product-description flex-col">
+                <p>Product description</p>
+                <textarea name="descrption" rows={6} id="" placeholder='write content here'></textarea>
+            </div>
+            <div className="add-catagory  flex-col">
+                <p>Product Ctegory</p>
+                <select name="category" id="">
+                    <option value="salad">salad</option>
+                    <option value="Rolls">Rolls</option>
+                    <option value="Deserts">Deserts</option>
+                    <option value="Sandwich">Sandwich</option>
+                    <option value="Cake">Cake</option>
+                    <option value="Pure">Pure</option>
+                    <option value="Pasta">Pasta</option>
+                    <option value="Noodles">Noodles</option>
+                </select>
+            </div>
+            <div className="add-price 
+            flex-col">
+                <p>Product Price</p>
+                <input type="Number" name="price" placeholder='$20' id="" />
+            </div>
+            <button type="submit" className='add-btn'>ADD</button>
+        </form>
+    </div>
   )
 }
 
